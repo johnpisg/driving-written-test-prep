25cbe79ecb0fda8661a00ffd5c389f7d6f8a2e8a
Jhon Pierre <johnpisg@gmail.com>
2013-11-03 15:40:36 -0400

    Basic Message
    Get started with phoneJs.

---
+++ JDriveTestPrep/JDriveTestPrep/index.js
@@ -0,0 +1,32 @@
+﻿window.JDriveTestPrep = {};
+$(function () {
+    JDriveTestPrep.app = new DevExpress.framework.html.HtmlApplication(
+    {
+        namespace: JDriveTestPrep,
+        defaultLayout: "myLayout"
+    }
+    );
+    JDriveTestPrep.app.router.register(":view/:name", { view: "home", name: "" });
+    JDriveTestPrep.app.navigate();
+});
+
+JDriveTestPrep.home = function () {
+    var viewModel = {
+        message: ko.observable('Welcome!'),
+        name: ko.observable(''),
+        sayHello: function () {
+            this.message("Hello " + this.name() + '!');
+        },
+        greet: function () {
+            JDriveTestPrep.app.navigate("greeting/" + this.name());
+        }
+    };
+    return viewModel;
+};
+
+JDriveTestPrep.greeting = function (params) {
+    var viewModel = {
+        message: 'Hello ' + params.name +'!',
+    };
+    return viewModel;
+};
